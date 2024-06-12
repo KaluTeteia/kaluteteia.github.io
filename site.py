a02d7a5ae9e1f0c8e2b2775647043adb7a6f6960
KaluTeteia <133132740+KaluTeteia@users.noreply.github.com>
2024-06-11 23:06:49 -0300

    Create site.py

---
+++ site.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# criar a primeira página do site
+# route -> kaluteteia.github.io/
+# função(O que exibir na página)
+
+@app.route('/')
+def inicio():
+    return 'Esse é meu primeiro site. Gostou?'
+
+# lançar o site
+app.run(debug = True)
